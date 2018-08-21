74bda0b69b77376e7bc5f73f01e020adca08e1a2
Christopher Barnes <senrabc@gmail.com>
2018-08-21 10:16:31 -0400

    added config example to pass token from config file and added TODO items for commandline arguments that need to be implemented

--- rc_api_tester.py
+++ rc_api_tester.py
@@ -2,8 +2,35 @@
 
 # Purpose: 
 
+# Version: 0.0.1
 
+# Author(s): See AUTHORS file in repo. All rights reserved.
+
+import configparser
 from urllib import request, parse
+
+# commandline arguments need to go here
+## TODO: ARG: log_file_name:  need arg for loggfile name, needs default 
+## TODO: ARG: log_file_path: need arg for log file location, needs default
+## TODO: ARG: verbose: need arg so output will goto stdout? or maybe that shoudl be
+##               default
+## TODO: ARG: help: needs to print help options
+## TODO: ARG: version: needs to pront version
+
+# parse config from config.ini
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+config.sections()
+token = config['DEFAULT']['Token']
+
+
+
+
+
+
+
+# Api testing code below
 # data
 data = parse.urlencode( {                                                                       
      'token': '<token config needs to go here>',                               
@@ -16,4 +43,5 @@ url = 'https://www.hcvtargetrc.org/api/'
 req =  request.Request(url, data=data)
 response = request.urlopen(req)
 
+# logging and options to goto stdout need to go here.
 print(response.read().decode('utf-8'))
